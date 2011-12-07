ae5256ee8bf05e470f2efcdf589c11bbfaefc51f
hardmarlin <hardmarlin@yahoo.com>
2011-12-07 12:41:10 -0300

    commit pmpi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pmpi::Application.config.secret_token = 'ae0f17b5eb3921f875f4fd8040cc105a319ce50ab9f405c94a7d17a2d23e5ee3c1384280ab0be9475171eec64238adbf961319d20512a6fb4cc9de556228c300'
